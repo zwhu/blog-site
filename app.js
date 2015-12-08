7b448375009accd14afe8ef84fb5a082791b0e5e
胡正伟 <331946763@qq.com>
2015-12-08 22:07:33 +0800

    删除无用代码

--- app.js
+++ app.js
@@ -14,18 +14,6 @@ app.use(errorHandling(app))
 // header 加入响应时间
 app.use(responseTime())
 
-app.use(function* (next) {
-
-  this.throw(401, 'access_denied', {error: 'hello,world!'})
-  yield next
-})
-
-app.use(function* (next) {
-  console.log(123)
-  yield next
-})
-
-
 let port = 8080
 app.listen(port, () => {
   console.log(`server is open: ${port}`)
